eb188ed7f68998e49b1f463f70dcd096746cb071
j-ornelas <johnbryan.ornelas@gmail.com>
2023-05-25 13:01:58 -0400

    add simple routing to app.js

--- l-systems-react/src/App.js
+++ l-systems-react/src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 import { Simple } from './pages/Simple';
 
 function App() {
   return (
     <div className="App">
-      <Simple />
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/simple">Simple Example</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/simple" element={<Simple />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
